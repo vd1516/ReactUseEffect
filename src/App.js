d534ef2ec32a517ff395c62695d1de74147cdc57
vd1516 <vrushabh.dhameliya1516@gmail.com>
2024-09-05 22:49:51 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  //1 Run on every render
+  useEffect(() => {
+    alert('Every Render Call')
+  })
+  
+  // let callData = () => {
+  //   alert("Call")
+  // }
+
+  //2 Run only one time
+  useEffect(() => {
+    alert('Render Call only one Time')
+  },[])
+
+  //3 Run When Update Pericular Data 
+  let [counter,updateCounter] = useState(0)
+  let updateData = () => {
+    updateCounter(counter+1)
+  }
+  useEffect(() => {
+    alert('Call When Counter Update')
+  },[counter])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {counter}
+      <button>Click</button>
+      <button onClick={updateData}>Counter</button>
     </div>
   );
 }
